f5cd6355608103dc7c5532bcf4172b711c2b23cc
Florian <florianhusson@hotmail.fr>
2021-10-15 19:02:03 +0200

    unity init

---
+++ minijam91/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    #region instance
+    private static GameManager instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(instance == null)
+                Debug.LogError("GameManager instance not found.");
+
+            return instance;
+        }
+    }
+    #endregion
+
+    #region GameStates
+
+    public enum GameStates
+    {
+        MainMenu,
+        InGame,
+        Pause,
+        Win,
+        Gameover,
+    }
+    public GameStates gameState;
+
+    /// <summary>
+    /// Change le <see cref="GameManager.GameState"/> par la valeur donnée, ou retourne le <see cref="GameManager.GameState"/> actuel
+    /// <para>
+    /// Note : la valeur de <see cref="GameManager.gameState"/> n'est modifiée qu'à la fin de l'appel des fonctions,
+    /// donc <see cref="GameManager.gameState"/> retournera l'ancienne valeur, et <value> value </value> retournera l'actuelle
+    /// </para>
+    /// </summary>
+    public GameStates GameState
+    {
+        set
+        {
+            switch(value)
+            {
+                case GameStates.MainMenu:
+
+                    break;
+
+                case GameStates.InGame:
+
+                    break;
+
+                case GameStates.Pause:
+
+                    break;
+
+                case GameStates.Win:
+
+                    break;
+
+                case GameStates.Gameover:
+
+                    break;
+
+                default:
+                    Debug.LogError(value + " state not found in switch statement.");
+                    break;
+            }
+
+            UIManager.Instance.WindowManager(value);
+
+            gameState = value;
+            
+        }
+        get => gameState;
+    }
+
+    #endregion
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
---
+++ minijam91/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    #region instance
+    private static UIManager instance;
+    public static UIManager Instance
+    {
+        get
+        {
+            if(instance == null)
+                Debug.LogError("UIManager instance not found.");
+
+            return instance;
+        }
+    }
+    #endregion
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    /// <summary>
+    /// Modifie l'UI suivant le nouveau GameState
+    /// <para>
+    /// Note : <paramref name="gameState"/> renvoie la nouvelle valeur, <see cref="GameManager.GameState"/> renvoie l'ancienne
+    /// </para>
+    /// </summary>
+    /// <param name="gameState"> the new state of game</param>
+    public void WindowManager(GameManager.GameStates gameState)
+    {
+        switch(gameState)
+        {
+            case GameManager.GameStates.MainMenu:
+
+                break;
+
+            case GameManager.GameStates.InGame:
+
+                break;
+
+            case GameManager.GameStates.Pause:
+
+                break;
+
+            case GameManager.GameStates.Win:
+
+                break;
+
+            case GameManager.GameStates.Gameover:
+
+                break;
+            default:
+                Debug.LogError(gameState + " state not found in switch statement.");
+                break;
+        }
+    }
+}
