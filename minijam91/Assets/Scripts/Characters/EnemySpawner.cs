def23265c5afef9d7708a5a78f21a91bd6e7686f
Florian <florianhusson@hotmail.fr>
2021-10-16 17:56:36 +0200

    Spawner

---
+++ minijam91/Assets/Scripts/Characters/EnemySpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> enemies;
+
+    [Header("Stats")]
+    [SerializeField] private float minSpawnTime;
+    [SerializeField] private float maxSpawnTime;
+
+    [SerializeField] private float speed;
+
+    [SerializeField] private Transform basePos;
+    [SerializeField] private Transform targetPos;
+    private Transform direction;
+
+    private bool moveToTarget;
+
+    private void Start()
+    {
+        if(basePos == null)
+            basePos = this.transform;
+
+        direction = targetPos;
+
+        moveToTarget = true;
+
+        SpawnEnemy();
+    }
+
+    private void Update()
+    {
+        Movements();
+    }
+
+    private void SpawnEnemy()
+    {
+        int randomEnemy = Random.Range(0, enemies.Count - 1);
+        Instantiate(enemies[randomEnemy], this.transform.position, Quaternion.identity);
+
+        float spawnTime = Random.Range(minSpawnTime, maxSpawnTime);
+        StartCoroutine(SpawnTimer(spawnTime));
+    }
+
+    private IEnumerator SpawnTimer(float time)
+    {
+        yield return new WaitForSeconds(time);
+        SpawnEnemy();
+    }
+
+    private void Movements()
+    {
+        this.transform.position = Vector2.MoveTowards(this.transform.position, direction.position, speed * Time.deltaTime);
+
+        if(this.transform.position.y <= targetPos.position.y)
+            direction = basePos;
+        else if (this.transform.position.y >= basePos.position.y)
+            direction = targetPos;
+    }
+}
